f1605766abdf6717aeefe23a3fcd9dc3fdc759c9
coder <coder160@2806-10a6-0022-cb8d-b55b-665a-5502-a131.ipv6.infinitum.net.mx>
2023-10-09 00:54:07 -0400

    update vite

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/aiencorto_blog/',
   plugins: [
     vue({
       template: { transformAssetUrls }
