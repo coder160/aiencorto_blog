33c0486617343156b7dbb57b136bf4909301d595
coder160 <coder160@google_colab.com>
2023-10-25 13:11:48 +0000

    MIA_POST_Octubre

--- src/assets/entradas.js
+++ src/assets/entradas.js
@@ -1,10 +1,3 @@
-import ejemplo_ESPACIAL from './post/base_espacial.json'
-import ejemplo_AI from './post/base_artificial.json'
-import ejemplo_ROBOTICA from './post/base_robotica.json'
-import ejemplo_SALUD from './post/base_medicinal.json'
-import ejemplo_AMBIENTE from './post/base_ambiental.json'
-import ejemplo_NEWS from './post/base_actualidad.json'
-import ejemplo_INVESTIGACION from './post/base_investigacion.json'
 import inteligencia_artificial from './post/1_inteligencia_artificial.json'
 import python from './post/4_python_lenguaje.json'
 import tensorflow from './post/5_tensorflow.json'
@@ -15,6 +8,7 @@ import diffusers from './post/9_diffusers.json'
 import matplotlib from './post/10_matplotlib.json'
 import pandas from './post/11_pandas.json'
 import RSS from './post/RSS.json'
+import streamlit from './post/Streamlit.json'
 
 
 export var Entradas = [
@@ -28,5 +22,6 @@ export var Entradas = [
     pipeline,    
     diffusers,
     RSS,
+    streamlit,
     
 ]
