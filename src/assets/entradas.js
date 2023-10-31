9085c2bf8e951271e54fb6367bb0194e4a8a8fdb
coder160 <coder160@google_colab.com>
2023-10-31 23:25:49 +0000

    MIA_POST_Octubre

--- src/assets/entradas.js
+++ src/assets/entradas.js
@@ -24,6 +24,25 @@ import google_colab from './post/GoogleColab.json'
 import vision_artificial from './post/Vision Artificial.json'
 
 import redes_neuronales from './post/Redes Neuronales.json'
+import superheroes from './post/Superhéroes Artificiales.json'
+import cempasuchil from './post/Flor de Cempasúchil.json'
+import gpt4_hackeado from './post/GPT4-Vision Hackeado.json'
+import james_webb from './post/James Webb Aniversario.json'
+import mision_xcrism from './post/Mision XCrism.json'
+import calentamiento_septiembre from './post/Calentamiento Septiembre23.json'
+import fiebre_oceanica from './post/Fiebre Oceánica.json'
+import sequia_espana from './post/Sequía Española.json'
+import descubrimiento_australia from './post/Descubrimiento Australia.json'
+import mundos_oceanicos from './post/Mundos Oceánicos.json'
+import marte from './post/Marte.json'
+import tierra from './post/Tierra.json'
+import astrobiologia from './post/AstroBiología.json'
+import titan from './post/Titan.json'
+import asteroides_troyanos from './post/Asteroides Troyanos.json'
+import europa from './post/Europa.json'
+
+
+
 
 export var Entradas = [
     inteligencia_artificial,
@@ -49,4 +68,20 @@ export var Entradas = [
     google_colab,
     vision_artificial,
     redes_neuronales,
+    superheroes,
+    cempasuchil,
+    gpt4_hackeado,
+    james_webb,
+    mision_xcrism,
+    calentamiento_septiembre,
+    fiebre_oceanica,
+    sequia_espana,
+    descubrimiento_australia,
+    mundos_oceanicos,
+    marte,
+    tierra,
+    astrobiologia,
+    titan,
+    asteroides_troyanos,
+    europa
     ]
